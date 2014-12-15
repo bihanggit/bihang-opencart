7eac91462fcd7ad4327ad365e2befeeabb8e6e4b
moxing <star2ko@gmail.com>
2014-12-15 14:33:57 +0800

    support callback

--- upload/catalog/controller/payment/oklink.php
+++ upload/catalog/controller/payment/oklink.php
@@ -7,8 +7,24 @@ class ControllerPaymentOklink extends Controller {
 		$this->load->model('checkout/order');
 
 		$order_info = $this->model_checkout_order->getOrder($this->session->data['order_id']);
-
 		if( $order_info['currency_code'] == 'USD' || $order_info['currency_code'] == 'CNY' || $order_info['currency_code'] == 'BTC' ){
+		    $data['oklink_confirm_url'] = $this->url->link('payment/oklink/confirm');
+		   	$data['error_warning'] = '';
+		}else{
+			$data['error_warning'] = $this->language->get('erro_currency');
+		}
+		if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/payment/oklink.tpl')) {
+			return $this->load->view($this->config->get('config_template') . '/template/payment/oklink.tpl', $data);
+		} else {
+			return $this->load->view('default/template/payment/oklink.tpl', $data);
+		}
+	}
+
+	public function confirm(){
+
+		    $this->load->model('checkout/order');
+
+		    $order_info = $this->model_checkout_order->getOrder($this->session->data['order_id']);		
 		    $this->load->model('payment/oklink');
 		    $client = $this->model_payment_oklink->getClinet();
 		    $params = array(
@@ -23,32 +39,27 @@ class ControllerPaymentOklink extends Controller {
 		    $result = $client->buttonsButton($params);
 	 	    if( $result->success == true ){
 		    	$button_id = $result->button->id;
-		    	$data['oklink_payment_url'] = 'https://www.oklink.com/merchant/mPayOrderStemp1.do?buttonid='.$button_id;
-		   		$data['error_warning'] = '';
+		    	$oklink_url = 'https://www.oklink.com/merchant/mPayOrderStemp1.do?buttonid='.$button_id;
+		    	header('Location: '.$oklink_url);
 		    }else{
 		    	$data['error_warning'] = $result->error;
 		    }
-		}else{
-			$data['error_warning'] = $this->language->get('erro_currency');
-		}
-		if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/payment/oklink.tpl')) {
-			return $this->load->view($this->config->get('config_template') . '/template/payment/oklink.tpl', $data);
-		} else {
-			return $this->load->view('default/template/payment/oklink.tpl', $data);
-		}
+		    return $this->load->view('default/template/payment/oklink.tpl', $data);
+
 	}
 
 	public function callback() {
 		if (($this->request->server['REQUEST_METHOD'] == 'POST')) {
 			$this->load->model('payment/oklink');
 			$client = $this->model_payment_oklink->getClinet();
-			$order = $client->getOrder();
-            if( $order && ($order->status == 'completed' || $order->status == 'mixpaid')){
-            	$this->redirect($this->url->link('checkout/success'));
-            }else{
-            	$this->load->language('payment/oklink');
-            	$data['error_warning'] = $this->language->get('payment_fail');
-            	$this->redirect($this->url->link('checkout/checkout'));
+            if( $client->checkCallback() ){
+				$order = file_get_contents('php://input');
+                $order = json_decode($order);
+	            if( $order && ($order->status == 'completed' )){
+				    $this->load->model('checkout/order');
+				    $this->model_checkout_order->addOrderHistory($order->custom, $this->config->get('config_order_status_id')); 
+				    header("HTTP/1.0 200 OK");
+	            }
             }
 		}
 	}
